6a812c046c54d2d320ce4a998ed0b79e1f53dde0
Mario Alvarado <96087936+m-alvaradox@users.noreply.github.com>
2025-06-13 11:23:59 -0500

    Algoritmo aprobación de un estudiante

---
+++ algoritmo-marioalvarado.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Estudiante
+{
+    string nombre = "Lucía";
+    float nota = 8.5f;
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Nota: " + nota);
+
+        if (nota >= 6)
+        {
+            Console.WriteLine("Ha aprobado el curso.");
+        }
+        else
+        {
+            Console.WriteLine("No ha aprobado el curso.");
+        }
+    }
+
+    public bool EstaAprobado()
+    {
+        return nota >= 7;
+    }
+
+    public static void Main(string[] args)
+    {
+        Estudiante estudiante = new Estudiante();
+        estudiante.MostrarDatos();
+
+        bool aprobado = estudiante.EstaAprobado();
+        Console.WriteLine("¿Aprobado?: " + aprobado);
+    }
+}
